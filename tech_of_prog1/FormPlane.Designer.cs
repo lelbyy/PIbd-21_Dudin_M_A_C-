ce436bdff88d50feb05d2208f5c80fb8692a20b4
lelbyy <beznikaxxx@mail.ru>
2020-11-23 20:07:58 +0400

    Lab-1 is ready

---
+++ tech_of_prog1/FormPlane.Designer.cs
@@ -0,0 +1,35 @@
+﻿namespace tech_of_prog1
+{
+	partial class FormPlane
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		
+
+		
+		
+		private System.Windows.Forms.PictureBox pictureBoxPlane;
+		private System.Windows.Forms.Button buttonCreate;
+		private System.Windows.Forms.Button buttonUp;
+		private System.Windows.Forms.Button buttonLeft;
+		private System.Windows.Forms.Button buttonDown;
+		private System.Windows.Forms.Button buttonRight;
+	}
+}
+
